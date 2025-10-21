604075d7fb3bc1bce868f733a156737c7de4986d
T <the.primeagen@gmail.com>
2025-10-21 16:56:02 -0700

    t

--- src/main/java/frc/robot/commands/ExampleCommand.java
+++ src/main/java/frc/robot/commands/ExampleCommand.java
@@ -26,7 +26,6 @@ public class ExampleCommand extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
   }
 
   // Called every time the scheduler runs while the command is scheduled.
