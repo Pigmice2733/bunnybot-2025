b68091f38ee5dc1e21f59c759f62efa2f0262124
Andrew <yeetes3000@gmail.com>
2025-11-04 16:30:14 -0800

    add shoot controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,77 +4,36 @@
 
 package frc.robot;
 
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  private final ExampleSubsystem exampleSubsystem;
-  private final Drivetrain drivetrain;
+    private final Drivetrain drivetrain;
 
-  private final CommandXboxController driver;
-  private final CommandXboxController operator;
-  private final Controls controls;
+    private final Shooter shooter = new Shooter();
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer() {
-    driver = new CommandXboxController(0);
-    operator = new CommandXboxController(1);
-    controls = new Controls(driver, operator);
+    private final CommandXboxController driver;
+    private final CommandXboxController operator;
+    private final Controls controls;
 
-    exampleSubsystem = new ExampleSubsystem();
-    drivetrain = new Drivetrain();
-    // Configure the trigger bindings
-    configureBindings();
-  }
+    public RobotContainer() {
+        driver = new CommandXboxController(0);
+        operator = new CommandXboxController(1);
+        controls = new Controls(driver, operator);
+    
+        drivetrain = new Drivetrain();
+        // Configure the trigger bindings
+        configureBindings();
+    }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be
-   * created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-   * an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-   * {@link
-   * CommandXboxController
-   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or
-   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(exampleSubsystem));
+    private void configureBindings() {
+        // Shoot when right trigger is pressed.
+        operator.rightTrigger().onTrue(shooter.shoot());
+    }
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
-    // pressed,
-    // cancelling on release.
-    driver.rightBumper().whileTrue(exampleSubsystem.exampleMethodCommand());
-  }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(exampleSubsystem);
-  }
+    public Command getAutonomousCommand() {
+        return null;
+    }
 }
