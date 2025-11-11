22880b0099dcef255baa27de8e707a4f678a2d9f
existence-mp3 <existence.mp3@gmail.com>
2025-11-10 16:02:13 -0800

    indexer documentation

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -23,6 +23,11 @@ public class Indexer {
         lowMotor.configure(indexerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
 
+    /**
+     * Sets the speed of both indexer motors
+     * 
+     * @param pct Value between -1 and 1 for the speed to be set to
+     */
     public void setIndexerSpeed(double pct) {
         if (Math.abs(pct) > 1)
             return;
@@ -30,19 +35,35 @@ public class Indexer {
         lowMotor.set(pct);
     }
 
+    /**
+     * @return Speed of the upper motor of the indexer
+     */
     public double getUpperIndexerSpeed() {
         return upMotor.get();
     }
 
+    /**
+     * @return Speed of the lower motor of the indexer
+     */
     public double getLowerIndexerSpeed() {
         return lowMotor.get();
     }
 
+    /**
+     * Sets the speed of both indexer motors to the default indexer speed. Can be
+     * set to go forwards or backwards.
+     * 
+     * @param fwd Boolean value of whether the motors are running forwards or
+     *            backwards
+     */
     public Command startIndexer(boolean fwd) {
         return Commands
                 .runOnce(() -> setIndexerSpeed(fwd ? IndexerConfig.INDEXER_SPEED : -IndexerConfig.INDEXER_SPEED));
     }
 
+    /**
+     * Sets the speed of both indexer motors to 0
+     */
     public Command stopIndexer() {
         return Commands.runOnce(() -> setIndexerSpeed(0));
     }
